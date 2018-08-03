2f20b454c60e6ef22cc7406f08ddbda537103221
ahmadgh7 <ghannam.as@gmail.com>
2018-08-03 16:04:42 +0300

    Corrected the mobile app name

--- BsMobile/Properties/AssemblyInfo.cs
+++ BsMobile/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BsMobile")]
+[assembly: AssemblyTitle("Babysafe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BsMobile")]
+[assembly: AssemblyProduct("Babysafe")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
